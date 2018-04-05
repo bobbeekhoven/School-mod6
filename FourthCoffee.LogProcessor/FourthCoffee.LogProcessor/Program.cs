64ce88cc6571f0b123897b61b486996460758d51
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-04-05 12:49:09 +0200

    Add files via upload

---
+++ FourthCoffee.LogProcessor/FourthCoffee.LogProcessor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthCoffee.LogProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logLocator = new LogLocator("C:\\Program Files\\Microsoft Learning\\20483\\Allfiles\\Mod06\\Democode\\Data\\Logs\\");
+            var logCombiner = new LogCombiner(logLocator);
+
+            logCombiner.CombineLogs("C:\\Program Files\\Microsoft Learning\\20483\\Allfiles\\Mod06\\Democode\\Data\\Logs\\CombinedLog.txt");
+
+        }
+    }
+}
